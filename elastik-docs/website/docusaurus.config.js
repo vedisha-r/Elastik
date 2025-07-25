a0c66b8a574c66056e3d859f06408861e4ba0f29
vedisha-r <vedisha.roopun@vgrgroup.mu>
2025-07-25 13:54:55 +0400

    Update today

---
+++ elastik-docs/website/docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Elastik Docs',
+  url: 'https://vedisha-r.github.io',
+  baseUrl: '/Elastik/',
+  organizationName: 'vedisha-r', // GitHub username
+  projectName: 'Elastik',     // GitHub repo name
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+};
